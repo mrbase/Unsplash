e4973e2e8e0a102a43250f4e1d60fdd93ac9a100
Hasse Ramlev Hansen <hasse@ramlev.dk>
2015-03-08 08:51:19 +0100

    Load the Goutte client correct

--- src/Unsplash/Command/DownloadCommand.php
+++ src/Unsplash/Command/DownloadCommand.php
@@ -10,6 +10,7 @@
 
 namespace Unsplash\Command;
 
+use Goutte\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -50,7 +51,7 @@ class DownloadCommand extends Command {
      * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $client = new Goutte\Client();
+        $client = new Client();
         $counter = 1;
 
         // Reset page and pager options if 'all' option is set.
